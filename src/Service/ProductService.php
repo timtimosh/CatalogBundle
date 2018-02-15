698161b4fb04172f96d4260d740006673df78a74
Владимир Тимошенко <vladimirtimosenko@MacBook-Air-Vladimir.local>
2018-02-15 17:45:10 +0200

    list and show

--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -15,14 +15,23 @@ class ProductService
 
     protected $classEntity;
     protected $router;
-
+    /**
+     * @var \Vich\UploaderBundle\Templating\Helper\UploaderHelper
+     */
+    protected $vichUploaderHelperService;
     /**
      * Page constructor.
      * @param EntityManager $entityManager
      * @param $classEntity string
      */
-    public function __construct(EntityManager $entityManager, $classEntity, \Symfony\Component\Routing\RouterInterface $router)
+    public function __construct(
+        EntityManager $entityManager,
+        $classEntity,
+        \Symfony\Component\Routing\RouterInterface $router,
+        \Vich\UploaderBundle\Templating\Helper\UploaderHelper $vichUploaderHelper
+    )
     {
+        $this->vichUploaderHelperService = $vichUploaderHelper;
         $this->em = $entityManager;
         $this->classEntity = $classEntity;
         $this->router = $router;
@@ -39,6 +48,33 @@ class ProductService
 
     public function getProductUrl(ProductInterface $entity):string{
         return $this->router->generate('catalog_product_show', array('slug' => $entity->getSlug()), UrlGeneratorInterface::ABSOLUTE_URL);
+    }
+
+    /**
+     * @param Product $entity
+     * @return string
+     */
+    public function getProductMainImgPath(ProductInterface $entity):string{
+        $image = null;
+        if(null !== $entity->getMainImage()) {
+            $image = $this->vichUploaderHelperService->asset($entity, 'mainImageFile');
+        }
+
+        if(null === $image){
+            $image = 'bundles/mttcatalog/images/placeholder.jpg';
+        }
+        return $image;
+    }
+    /**
+     * @param Product $entity
+     * @return float
+     */
+    public function getPrice(ProductInterface $entity):float{
+        return (float) $entity->getPrice();
+    }
+
+    public function formatPrice(float $price){
+        return $price.' грн';
     }
 
 }
